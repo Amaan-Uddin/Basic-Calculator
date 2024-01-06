b14d9002b020b8074db8f936e81771ae8929239a
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2024-01-06 08:28:57 +0530

    Initial setup complete

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const calciNumbers = document.querySelectorAll(".calciNumber");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const display = document.getElementById("display");
+const backButton = document.getElementById("backButton");
+
+let displayString = "";
+let operator = "";
+let operand1 = null;
+
+function arithmeticOperation(op1, op2, op) {
+  if (op == "+") return op1 + op2;
+  else if (op == "*") return op1 * op2;
+  else if (op == "-") return op1 - op2;
+  else if (op == "/") return op1 / op2;
+  else return op1 % op2;
+}
+
+backButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  displayString = displayString.slice(0, displayString.length - 1);
+  display.value = displayString.length == 0 ? null : displayString;
+});
+
+calciNumbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    e.preventDefault();
+    displayString += e.target.dataset.val;
+    display.value = displayString;
+  });
+});
+
+operators.forEach((optr) => {
+  optr.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (displayString.length) {
+      operator = e.target.dataset.val;
+      operand1 = display.value;
+      displayString = "";
+      display.value = null;
+    }
+  });
+});
+
+equals.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(operand1, operator, display.value);
+  if (operand1 !== null && operator.length && displayString.length) {
+    display.value = arithmeticOperation(Number(operand1), Number(display.value), operator);
+    displayString = String(display.value);
+  }
+});
